5f12bda71b0b934c8af49d40305fa081da9ff22d
muhammet <mavibirsaat@gmail.com>
2025-05-12 09:48:55 +0300

    manual

--- prj_movie/urls.py
+++ prj_movie/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app_watchlist.urls")),
+    path("movie/", include("app_watchlist.urls")),
 ]
